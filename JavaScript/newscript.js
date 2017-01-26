214cbe385fe3bdd4c7265f487f6ce0a303ff27ba
Luís Filipe Soutello <lsoutello@daitangroup.com>
2017-01-26 17:18:18 -0200

    modal structure and new loader

--- JavaScript/newscript.js
+++ JavaScript/newscript.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
   var list = [];
 
-  $.get('http://pokeapi.co/api/v2/pokemon/')
-  .then(function(response1) {
-    return response1.results.map(function(pokemon) {
-      return $.get(pokemon.url).then(function(response2) {
-        var pokemon = {
-          name: response2.name,
-          id: response2.id,
-          sprite: response2.sprites.front_default
-        };
-        list[pokemon.id-1] = pokemon;
-        console.log(list.length);
-      }).promise();
-    });
-  }).then(function(promises) {
-    $.when(promises).then(function() {
-      debugger;
-      list.forEach( function(pokemon) {
-        $('.poke_list').append('<div class="col-md-3"><div class="card"><div class="row"><div class="col-md-12"><img src="' + sprite + '" class="card-image"></img></div></div><div class="row"><div class="col-md-12"><h2 class="text-center">#' + id + ' ' + name + '</h2></div></div></div></div>');
+  function loadPokemons() {
+    $('body').append('<div class="loader"><img src="images/pikachu.gif"></img></div>');
+    $.get('http://pokeapi.co/api/v2/pokemon/')
+    .then(function(response1) {
+      return response1.results.map(function(pokemon) {
+        return $.get(pokemon.url).then(function(response2) {
+          var pokemon = {
+            name: response2.name,
+            id: response2.id,
+            sprite: response2.sprites.front_default
+          };
+          list[pokemon.id-1] = pokemon;
+          console.log(list.length);
+        }).promise();
       });
+    }).then(function(promises) {
+      return $.when.apply($, promises).then(function() {
+        list.forEach( function(pokemon) {
+          $('.poke_list').append('<div class="col-md-3"> <div class="card" data-toggle="modal" data-target="#pokeModal"> <div class="row"> <div class="col-md-12"> <img src="' + pokemon.sprite + '" class="card-image"></img> </div> </div> <div class="row"> <div class="col-md-12"> <h2 class="text-center">#' + pokemon.id + ' ' + pokemon.name + '</h2> </div> </div> </div> </div>');
+        });
+      });
+    }).catch(function() {
+      $('body').append('<h1>ERRO!</h1>');
+    }).always(function() {
+      $('.loader').detach();
     });
-  });
+  }
+
+  loadPokemons();
 });
